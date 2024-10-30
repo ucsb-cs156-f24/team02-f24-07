cdb64b533ea83a6123d86bc5b0dbf0e655babc04
Logan Detrick <logandetrick@ucsb.edu>
2024-10-30 11:29:33 -0700

    ld - tabs -> spaces

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,31 +1,34 @@
 const ucsbOrganizationFixtures = {
   oneOrganization: [
-	{
-	  orgCode: "ZPR",
-	  orgTranslationShort: "ZETA PHI RHO",
-	  orgTranslation: "ZETA PHI RHO at UCSB",
-	  inactive: true,
-	},
+    {
+      orgCode: "ZPR",
+      orgTranslationShort: "ZETA PHI RHO",
+      orgTranslation: "ZETA PHI RHO at UCSB",
+      inactive: true,
+    },
   ],
+
   threeOrganizations: [
-	{
-	  orgCode: "ACM",
-	  orgTranslationShort: "ACM",
-	  orgTranslation: "ACM @ UCSB",
-	  inactive: false,
-	},
-	{
-	  orgCode: "osli",
-	  orgTranslationShort: "STUD LIFE",
-	  orgTranslation: "OFFICE OF STUDENT LIFE",
-	  inactive: false,
-	},
-	{
-	  orgCode: "KRC",
-	  orgTranslationShort: "KOREAN RADIO CL",
-	  orgTranslation: "KOREAN RADIO CLUB",
-	  inactive: true,
-	},
+    {
+      orgCode: "ACM",
+      orgTranslationShort: "ACM",
+      orgTranslation: "ACM @ UCSB",
+      inactive: false,
+    },
+
+    {
+      orgCode: "osli",
+      orgTranslationShort: "STUD LIFE",
+      orgTranslation: "OFFICE OF STUDENT LIFE",
+      inactive: false,
+    },
+
+    {
+      orgCode: "KRC",
+      orgTranslationShort: "KOREAN RADIO CL",
+      orgTranslation: "KOREAN RADIO CLUB",
+      inactive: true,
+    },
   ],
 };
 
