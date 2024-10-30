bc78a85e243d8e12a400b4448e599068302f600b
Logan Detrick <logandetrick@ucsb.edu>
2024-10-30 11:10:37 -0700

    ld - added fixture for ucsbOrganization

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,33 @@
+const ucsbOrganizationFixtures = {
+	oneOrganization: [
+		{
+			orgCode: "ZPR",
+			orgTranslationShort: "ZETA PHI RHO",
+			orgTranslation: "ZETA PHI RHO at UCSB",
+			inactive: true,
+		},
+	],
+	threeOrganizations: [
+		{
+			orgCode: "ACM",
+			orgTranslationShort: "ACM",
+			orgTranslation: "ACM @ UCSB",
+			inactive: false,
+		},
+		{
+			orgCode: "osli",
+			orgTranslationShort: "STUD LIFE",
+			orgTranslation: "OFFICE OF STUDENT LIFE",
+			inactive: false,
+		},
+		{
+			orgCode: "KRC",
+			orgTranslationShort: "KOREAN RADIO CL",
+			orgTranslation: "KOREAN RADIO CLUB",
+			inactive: true,
+		},
+	],
+};
+
+export { ucsbOrganizationFixtures };
+  
