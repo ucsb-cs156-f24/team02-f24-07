a74a1026443a974cb6ac949312d35e0898c208aa
Logan Detrick <logandetrick@ucsb.edu>
2024-11-07 01:05:40 -0800

    ld - remove duplicate menuitemreview in navbar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -72,17 +72,14 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsborganization">
                     UCSB Organizations
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/placeholder">
-                    Placeholder
-                  </Nav.Link>
                   <Nav.Link as={Link} to="/helprequests">
                     Help Requests
                   </Nav.Link>
                   <Nav.Link as={Link} to="/menuitemreview">
                     MenuItemReview
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/menuitemreview">
-                    MenuItemReview
+                  <Nav.Link as={Link} to="/placeholder">
+                    Placeholder
                   </Nav.Link>
                 </>
               ) : (
