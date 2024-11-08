6eb5b128d7367fb14a2599cdf526c32a94ed2ced
Logan Detrick <logandetrick@ucsb.edu>
2024-11-07 17:50:49 -0800

    ld - make the table be in the same storybook folder as the form

--- frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.js
+++ frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.js
@@ -3,7 +3,7 @@ import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
 import HelpRequestForm from "main/components/HelpRequests/HelpRequestForm";
 
 export default {
-  title: "components/HelpRequest/HelpRequestForm",
+  title: "components/HelpRequests/HelpRequestForm",
   component: HelpRequestForm,
 };
 
